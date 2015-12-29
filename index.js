8511b943ff742ef828750650a835c45c55c89501
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-29 19:03:30 +0200

    fix

--- index.js
+++ index.js
@@ -80,6 +80,8 @@ module.exports = {
         gmAPI.directions(util.pickInputs(step, pickInputs), function(err, result) {
             if (err)
                 this.fail(err);
+            if (result.error_message)
+                this.fail(result.error_message);
             else
                 this.complete(util.pickOutputs(result, pickOutputs));
         }.bind(this));
